5e01d41b426afb40c3d1dd865032d481bf0d3feb
London Kasper <lckasper@smu.edu>
2023-05-03 19:13:08 -0500

    Switched to a FIxedUpdate

--- testproject/Assets/Scripts/lookAt.cs
+++ testproject/Assets/Scripts/lookAt.cs
@@ -8,7 +8,7 @@ public class lookAt : MonoBehaviour
     // Start is called before the first frame update
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         transform.LookAt(target);
     }
